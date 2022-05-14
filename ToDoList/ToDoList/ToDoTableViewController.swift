bf7cfef125f658c6f00d21082ad5fefa48ca494f
yk-jp <k82.y12@gmail.com>
2022-05-14 14:16:13 -0700

    finish part3

--- ToDoList/ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -19,6 +19,7 @@ class ToDoTableViewController: UITableViewController {
         } else {
             toDos = ToDo.loadSampleToDos()
         }
+        navigationItem.leftBarButtonItem = editButtonItem
         
     }
     
@@ -39,26 +40,19 @@ class ToDoTableViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    override func tableView(_ tableView: UITableView, commit
+       editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath:
+       IndexPath) {
+        if editingStyle == .delete {
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+   
     
     /*
      // Override to support rearranging the table view.
