99a447e4694fd7152bedda6702b3a4a15e545687
yk-jp <k82.y12@gmail.com>
2022-05-15 00:16:35 -0700

    finish part7

--- ToDoList/ToDoList/ToDoDetailTableViewController.swift
+++ ToDoList/ToDoList/ToDoDetailTableViewController.swift
@@ -24,10 +24,19 @@ class ToDoDetailTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        dueDateDatePicker.date = Date().addingTimeInterval(24*60*60)
-   
-        updateDueDateLabel(date: dueDateDatePicker.date)
-
+        let currentDueDate: Date
+        if let toDo = toDo {
+          navigationItem.title = "To-Do"
+          titleTextField.text = toDo.title
+          isCompleteButton.isSelected = toDo.isComplete
+          currentDueDate = toDo.dueDate
+          notesTextView.text = toDo.notes
+        } else {
+          currentDueDate = Date().addingTimeInterval(24*60*60)
+        }
+        
+        dueDateDatePicker.date = currentDueDate
+        updateDueDateLabel(date: currentDueDate)
         updateSaveButtonState()
     }
     
@@ -74,8 +83,15 @@ class ToDoDetailTableViewController: UITableViewController {
         let dueDate = dueDateDatePicker.date
         let notes = notesTextView.text
         
-        toDo = ToDo(title: title, isComplete: isComplete,
-           dueDate: dueDate, notes: notes)
+        if toDo != nil {
+                toDo?.title = title
+                toDo?.isComplete = isComplete
+                toDo?.dueDate = dueDate
+                toDo?.notes = notes
+            } else {
+                toDo = ToDo(title: title, isComplete: isComplete,
+                   dueDate: dueDate, notes: notes)
+            }
     }
     
     func updateDueDateLabel(date: Date) {
--- ToDoList/ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -53,16 +53,39 @@ class ToDoTableViewController: UITableViewController {
         }
     }
    
+    @IBSegueAction func toDoEdit(_ coder: NSCoder, sender: Any?) -> ToDoDetailTableViewController? {
+        let detailController = ToDoDetailTableViewController(coder: coder)
+       
+        guard let cell = sender as? UITableViewCell,
+              let indexPath = tableView.indexPath(for: cell) else {
+            return detailController
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        detailController?.toDo = toDos[indexPath.row]
+        
+        return detailController
+    }
+    
     @IBAction func unwindToToDoList(segue: UIStoryboardSegue) {
         guard segue.identifier == "saveUnwind" else { return }
         let sourceViewController = segue.source as!
         ToDoDetailTableViewController
         
         if let toDo = sourceViewController.toDo {
-            let newIndexPath = IndexPath(row: toDos.count, section: 0)
+            if let indexOfExistingToDo = toDos.firstIndex(of: toDo) {
+                toDos[indexOfExistingToDo] = toDo
+                tableView.reloadRows(at: [IndexPath(row: indexOfExistingToDo,
+                                                    section: 0)], with: .automatic)
+            } else {
+                let newIndexPath = IndexPath(row: toDos.count, section: 0)
+                
+                toDos.append(toDo)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
             
-            toDos.append(toDo)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            
         }
     }
 
